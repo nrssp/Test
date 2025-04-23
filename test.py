9f5711cb455c31a95c581b45212c8f2687d9d3dd
nrssp <nrssp@hotmail.com>
2025-04-23 09:36:34 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -60,7 +60,7 @@ if not all(col in df.columns for col in required_columns):
 st.markdown("""
     <div style='display: flex; align-items: center; gap: 20px;'>
         <img src='https://dxugi372p6nmc.cloudfront.net/spdk/current/64x64/8391/teamlogo.png' width='60'>
-        <h1 style='margin: 0; color: #011a8b;'>F.C. København – Superliga Tabeltest</h1>
+        <h1 style='margin: 0; color: #011a8b;'>F.C. København – Superliga Tabel</h1>
     </div>
     <hr style='margin-top: 10px; margin-bottom: 30px; border: 1px solid #011a8b;'>
 """, unsafe_allow_html=True)
